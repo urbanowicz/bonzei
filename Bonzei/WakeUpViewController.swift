739044c38f9e71ccb03ca99a526a1eefcd40061f
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-04-15 13:12:32 +0200

    Building the outline. Added the 'Wake Up' View

--- Bonzei/WakeUpViewController.swift
+++ Bonzei/WakeUpViewController.swift
@@ -11,19 +11,17 @@ import UIKit
 class WakeUpViewController: UIViewController {
     
     @IBOutlet weak var setAlarmButton: UIButton!
-    @IBOutlet weak var firstLabel: UILabel!
-    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var setFirstAlarmButton: UIButton!
+    @IBOutlet weak var wakeUpLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstLabel.isHidden = false
-        secondLabel.isHidden = true
+
         // Do any additional setup after loading the view.
     }
 
     @IBAction func setAlarmButtonPressed(_ sender: UIButton) {
-        firstLabel.isHidden.toggle()
-        secondLabel.isHidden.toggle()
+
     }
     
 }
