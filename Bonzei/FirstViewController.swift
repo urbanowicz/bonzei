563ba80e000b45ee53ea7eef8b194e692657d781
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-04-15 11:17:27 +0200

    Initial Commit

---
+++ Bonzei/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Bonzei
+//
+//  Created by Tomasz on 15/04/2020.
+//  Copyright © 2020 bonzei.app. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
