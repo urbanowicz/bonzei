aabd4cbe2669cf0747483455529fd2c9aa6022c8
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-04-20 19:38:13 +0200

    Added 'SetMelodyViewController' with a basic view

---
+++ Bonzei/SetMelodyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SetMelodyViewController.swift
+//  Bonzei
+//
+//  Created by Tomasz on 20/04/2020.
+//  Copyright © 2020 bonzei.app. All rights reserved.
+//
+
+import UIKit
+
+class SetMelodyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
