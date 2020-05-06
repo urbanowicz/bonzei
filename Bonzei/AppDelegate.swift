cc68b4aa3f70506d81eb669b164dbe624d3f5809
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-05-06 16:38:45 +0200

    Implement UNUserNotificationCenterDelegate

--- Bonzei/AppDelegate.swift
+++ Bonzei/AppDelegate.swift
@@ -10,12 +10,14 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
 
@@ -31,6 +33,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    // MARK: - UNUserNotificationCenterDelegate
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        print("Scheduler::WillPresent: \(notification.request.content.body)")
+        
+        completionHandler([.alert, .badge, .sound,])
     }
 
     // MARK: - Core Data stack
