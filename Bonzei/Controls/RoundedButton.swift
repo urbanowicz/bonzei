cc888fbdf79ed60e9cbda4f59a4b31aff0df4760
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-05-26 18:26:52 +0200

    Applying style to WakeUpView

--- Bonzei/Controls/RoundedButton.swift
+++ Bonzei/Controls/RoundedButton.swift
@@ -14,13 +14,44 @@ import UIKit
 @IBDesignable
 class RoundedButton: UIButton {
     
-    /// Color of the circle in the background
-    @IBInspectable var cornerRadius:Float = 0.0
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+            self.clipsToBounds = true
+        }
+    }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        layer.cornerRadius = CGFloat(cornerRadius)
-        clipsToBounds = true
+    let gradientLayer = CAGradientLayer()
+    
+    @IBInspectable
+    var topGradientColor: UIColor? {
+        didSet {
+            setGradient(topGradientColor: topGradientColor, bottomGradientColor: bottomGradientColor)
+        }
+    }
+    
+    @IBInspectable
+    var bottomGradientColor: UIColor? {
+        didSet {
+            setGradient(topGradientColor: topGradientColor, bottomGradientColor: bottomGradientColor)
+        }
+    }
+    
+    private func setGradient(topGradientColor: UIColor?, bottomGradientColor: UIColor?) {
+        if let topGradientColor = topGradientColor, let bottomGradientColor = bottomGradientColor {
+            gradientLayer.frame = bounds
+            gradientLayer.colors = [topGradientColor.cgColor, bottomGradientColor.cgColor]
+            gradientLayer.borderColor = layer.borderColor
+            gradientLayer.borderWidth = layer.borderWidth
+            gradientLayer.cornerRadius = layer.cornerRadius
+            layer.insertSublayer(gradientLayer, at: 0)
+        } else {
+            gradientLayer.removeFromSuperlayer()
+        }
+    }
+    
+    override public func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        gradientLayer.frame = bounds
     }
 }
