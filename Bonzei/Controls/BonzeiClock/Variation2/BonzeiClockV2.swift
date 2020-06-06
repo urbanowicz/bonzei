b0de119424358b886849d7d84b980d4124e8c2bd
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-06-06 19:51:11 +0200

    Dismiss/Snooze Alarm View

---
+++ Bonzei/Controls/BonzeiClock/Variation2/BonzeiClockV2.swift
@@ -0,0 +1,33 @@
+//
+//  BonzeiClockV2.swift
+//  Bonzei
+//
+//  Created by Tomasz on 06/06/2020.
+//  Copyright © 2020 bonzei.app. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BonzeiClockV2: UIView {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    private func commonInit() {
+
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        ClockFaceAndDots.drawCanvas1(frame: rect, resizing: .aspectFit)
+    }
+}
