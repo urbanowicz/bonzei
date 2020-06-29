1380b8ee5528c0c05b40dbf65fb2244f6969d0aa
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-06-29 14:01:51 +0200

    Separate 'Learn' and 'AlarmClock' code

---
+++ Bonzei/Learn/ViewControllers/ArticlesCollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ArticlesCollectionViewController.swift
+//  Bonzei
+//
+//  Created by Tomasz on 29/06/2020.
+//  Copyright © 2020 bonzei.app. All rights reserved.
+//
+
+import UIKit
+
+class ArticlesCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
