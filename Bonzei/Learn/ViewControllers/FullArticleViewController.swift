9a49797c6ddb0c3f94810db749f7e07193026230
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-07-03 16:57:43 +0200

    Remove webview because it is causing occasional crashes

--- Bonzei/Learn/ViewControllers/FullArticleViewController.swift
+++ Bonzei/Learn/ViewControllers/FullArticleViewController.swift
@@ -13,14 +13,14 @@ class FullArticleViewController: UIViewController {
     
     var article: Article?
     
-    @IBOutlet weak var webView: WKWebView!
+   // @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let article = self.article {
-            webView.loadHTMLString(article.text, baseURL: nil)
-        }
+//        if let article = self.article {
+//            webView.loadHTMLString(article.text, baseURL: nil)
+//        }
         // Do any additional setup after loading the view.
     }
     
