84cba8a6dde94c8458f3a7ad7f599a5692b31952
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-07-04 09:23:11 +0200

    App Version

---
+++ Bonzei/AppVersion.swift
@@ -0,0 +1,18 @@
+//
+//  AppVersion.swift
+//  Bonzei
+//
+//  Created by Tomasz on 04/07/2020.
+//  Copyright © 2020 bonzei.app. All rights reserved.
+//
+
+import Foundation
+
+
+public func get_previous_app_version() -> Int64 {
+    return 0
+}
+
+public func set_previous_app_version(version :Int64) {
+    
+}
