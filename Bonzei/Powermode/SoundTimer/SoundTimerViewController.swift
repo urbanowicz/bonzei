96fbcc4f256060f0ce74576e325166bf2bc4a21a
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-08-06 11:02:11 +0200

    SoundTimerViewController

---
+++ Bonzei/Powermode/SoundTimer/SoundTimerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SoundTimerViewController.swift
+//  Bonzei
+//
+//  Created by Tomasz on 06/08/2020.
+//  Copyright © 2020 bonzei.app. All rights reserved.
+//
+
+import UIKit
+
+class SoundTimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
