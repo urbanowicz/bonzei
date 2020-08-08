ac770a5b4e5d00c0d77e213a119703989ecafa11
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-08-08 20:03:39 +0200

    SoundTimerDonveViewController

--- Bonzei/Powermode/SoundTimer/SoundTimerDoneViewController.swift
+++ Bonzei/Powermode/SoundTimer/SoundTimerDoneViewController.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class SoundTimerDoneViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    public var backgroundTopColor = #colorLiteral(red: 0.1377221048, green: 0.249750644, blue: 0.2173544168, alpha: 1) {
+        didSet {
+            if let gradientView = self.view as? GradientView {
+                gradientView.topColor = backgroundTopColor
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public var backgroundBottomColor = #colorLiteral(red: 0.1411813796, green: 0.3443938792, blue: 0.2596455514, alpha: 1) {
+        didSet {
+            if let gradientView = self.view as? GradientView  {
+                gradientView.bottomColor = backgroundBottomColor
+            }
+        }
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        backgroundTopColor = #colorLiteral(red: 0.1377221048, green: 0.249750644, blue: 0.2173544168, alpha: 1)
+        backgroundBottomColor = #colorLiteral(red: 0.1411813796, green: 0.3443938792, blue: 0.2596455514, alpha: 1)
+    }
 
 }
