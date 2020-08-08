6301632f28afcdff9d5ea9dc8e609df0bda122ce
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2020-08-08 20:58:56 +0200

    New melodies

--- Bonzei/WakeUp/Models/Melody.swift
+++ Bonzei/WakeUp/Models/Melody.swift
@@ -11,18 +11,14 @@ import Foundation
 /// A static array of names of melodies.
 ///
 /// For every melody name in the array there must be a corresponding `.mp3` file the `SoundFiles` folder.
-let melodies = ["Ambient Waves",
-                "Coast Cliffs",
-                "Ocean Horizon",
-                "Sunriser",
-                "Early Riser",
+let melodies = ["5AM Club",
+                "Bellflowers Dew",
+                "Crystal Vision",
+                "Earth Tribe",
                 "Endorphin Birds",
-                "LIPIEC_01",
-                "LIPIEC_02",
-                "LIPIEC_03",
-                "LIPIEC_04",
-                "LIPIEC_05",
-                "LIPIEC_06",
-                "LIPIEC_07",
-                "LIPIEC_08",
-                "LIPIEC_09"]
+                "Into The Source",
+                "Ocean Cliffs",
+                "Peace of Mind",
+                "Sea Horizon",
+                "Sky Sailing",
+                "Stellar Outflow"]
